fc9bd5bd8aa090ce53f31ca867ac0104dfdccb7b
jaweber64 <jaWeber@janets-mbp.attlocal.net>
2016-09-17 08:36:54 -0500

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/citygude/Pier.java
@@ -0,0 +1,13 @@
+package net.androidbootcamp.citygude;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pier extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pier);
+    }
+}
